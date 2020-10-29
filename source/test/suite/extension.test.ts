d4ea2fe7b05dc54da32e5d1b1f436a59e043a631
道化師 <645907+wraith13@users.noreply.github.com>
2020-10-30 04:50:06 +0900

    🚧 プロジェクトの構成を最新の yo code で生成されるヤツに合わせて更新

--- source/test/suite/extension.test.ts
+++ source/test/suite/extension.test.ts
@@ -1,20 +1,13 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-//import * as vscode from 'vscode';
-import * as ZoomBar from '../source/extension';
+import * as vscode from 'vscode';
+import * as ZoomBar from '../../extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+suite('Extension Test Suite', () => {
+    vscode.window.showInformationMessage('Start all tests.');
 
-    // Defines a Mocha unit test
     test("zoombar", () => {
         assert.equal(100.0, ZoomBar.ZoomBar.roundZoom(99.999));
         assert.equal(100.0, ZoomBar.ZoomBar.roundZoom(100.000));
